44e5b95f828d5ec1bd4142eff631e7d6462a3db8
Kamalkant <er.kamal.singhal@gmail.com>
2020-08-10 23:12:18 +0530

    Update samples.py

--- samples.py
+++ samples.py
@@ -16,3 +16,25 @@ for i in range(len(l3)):
                 result.append(l3[j])
                 
 print (result)
+
+# find the smallest number in python list without using sort or min function
+# 2. sub problem :- sorting required ? Not exactly.
+# 3. sub problem :- min required, yes for sorting and the given problem
+
+## below is the function to get the smallest number
+my_list = [-15, -26, 15, 1, 23, -64, 23, 76]
+def smallest(l):
+    mySmall = l[0]
+    for num in l:
+        if mySmall > num:
+            mySmall = num
+    return mySmall
+
+
+result_sorted = []
+for i in range(len(my_list)):
+    smaller = smallest (my_list)
+    result_sorted.append(smaller)
+    my_list.remove(smaller)
+
+print (result_sorted)
